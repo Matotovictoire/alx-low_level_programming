3d5d7369710aaa36ce588de5d7bc18c6cb21c185
Matotovictoire <matotovictoire@gmail.com>
2023-12-20 17:18:57 -0800

    6-abs.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,11 +3,9 @@
 /**
  * print_sign - Prints the sign of a number.
  * @n: The number of which the sign will be printed.
- *
- * Return: 1 if the number is greater than zero,
- *         0 if the number is zero,
- *         -1 if the number is less than zero.
+ * Return: 1 for positive num,  -1 for negative num or zero for anuthing else
  */
+
 int print_sign(int n)
 {
 	if (n > 0)
